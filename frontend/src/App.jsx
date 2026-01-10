ef630e45bd33560308b57d2bd0511a5614999d33
JBRUL254 <josiahokere13@gmail.com>
2026-01-10 10:49:53 +0300

    Set up routing for main application pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Deposit from "./pages/Deposit";
+import Withdraw from "./pages/Withdraw";
+import Loans from "./pages/Loans";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/deposit" element={<Deposit />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+        <Route path="/loans" element={<Loans />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
